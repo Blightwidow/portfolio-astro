b192b5388715a752af813d47c0a52452121efc2a
Theo Dammaretz <theo@dammaretz.fr>
2024-07-18 14:17:28 +0200

    feat: improve lighthouse score

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,16 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dammaretz.fr",
+  trailingSlash: "never",
   output: "static",
   integrations: [sitemap()],
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  experimental: {
+    directRenderScript: true,
+    contentCollectionCache: true,
+    clientPrerender: true,
+  }
 });
